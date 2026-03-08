042bb4748090849d42c85e90295d6dcb7c40c82c
Jan Kaczmarski <jkaczmarski@splunk.com>
2026-03-08 13:36:41 +0100

    client and server working without threadpool

--- distributed_systems/lab1/client.c
+++ distributed_systems/lab1/client.c
@@ -1,76 +1,202 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>      // dla close()
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/select.h>
+#include <errno.h>
 
-int sock;
-char buffer[1024];
+#define CMD_MEDIA "U "
+#define CMD_MULTICAST "M "
+#define MULTICAST_IP "239.0.0.1"
+#define MULTICAST_PORT 9000
+#define BUF_SIZE 2048
+
+volatile sig_atomic_t keep_running = 1;
+
+int sockStream = -1;
+int sockDgram = -1;
+int sockMulticast = -1;
+
+struct sockaddr_in server_addr;
+struct sockaddr_in mc_dest_addr;
+
+void cleanup() {
+    if (sockDgram != -1) {
+        close(sockDgram);
+        sockDgram = -1;
+    }
+    if (sockStream != -1) {
+        close(sockStream);
+        sockStream = -1;
+    }
+    if (sockMulticast != -1) {
+        close(sockMulticast);
+        sockMulticast = -1;
+    }
+}
 
 void sigint_handler(int sig) {
-    (void)sig; // int sig to satisfy func signature
+    (void)sig; 
+    keep_running = 0;
+}
 
-    close(sock);
-    printf("\nClient shutting down.\n");
-    exit(0);
+int setup_tcp(int port, const char *address) {
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(address);
+
+    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        perror("signal");
+        cleanup();
+        return 1;
+    }
+
+    // Ignore SIGPIPE — prevents crash if server disconnects while client is mid-send()
+    signal(SIGPIPE, SIG_IGN);
+
+    sockStream = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockStream == -1) {
+        perror("socket stream");
+        return 1;
+    }
+    if (connect(sockStream, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        cleanup();
+        return 1;
+    }
+
+    return 0;
+}
+
+int setup_udp() {
+    struct sockaddr_in local_addr;
+    socklen_t local_len = sizeof(local_addr);
+    
+    if (getsockname(sockStream, (struct sockaddr *)&local_addr, &local_len) == -1) {
+        perror("getsockname failed");
+        cleanup();
+        return 1;
+    }
+    
+    sockDgram = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockDgram == -1) {
+        perror("socket dgram");
+        return 1;
+    }
+    
+    if (bind(sockDgram, (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1) {
+        perror("bind udp failed");
+        cleanup();
+        return 1;
+    }
+
+    return 0;
+}
+
+int setup_multicast() {
+    sockMulticast = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockMulticast == -1) {
+        perror("multicast socket failed");
+        cleanup();
+        return 1;
+    }
+
+    int opt_mc = 1;
+    setsockopt(sockMulticast, SOL_SOCKET, SO_REUSEADDR, &opt_mc, sizeof(opt_mc));
+    setsockopt(sockMulticast, SOL_SOCKET, SO_REUSEPORT, &opt_mc, sizeof(opt_mc));
+    
+    struct sockaddr_in mc_local_addr;
+    memset(&mc_local_addr, 0, sizeof(mc_local_addr));
+    mc_local_addr.sin_family = AF_INET;
+    mc_local_addr.sin_port = htons(MULTICAST_PORT);
+    mc_local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockMulticast, (struct sockaddr *)&mc_local_addr, sizeof(mc_local_addr)) == -1) {
+        perror("bind multicast failed");
+        cleanup();
+        return 1;
+    }
+
+    struct ip_mreq mreq;
+    mreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_IP);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    if (setsockopt(sockMulticast, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) == -1) {
+        perror("multicast membership failed");
+    }
+
+    memset(&mc_dest_addr, 0, sizeof(mc_dest_addr));
+    mc_dest_addr.sin_family = AF_INET;
+    mc_dest_addr.sin_port = htons(MULTICAST_PORT);
+    mc_dest_addr.sin_addr.s_addr = inet_addr(MULTICAST_IP);
+    
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    // Odpalamy np: ./client 127.0.0.1 8080
     if (argc != 4) {
         printf("Usage: %s <server_ip> <server_port> <username>\n", argv[0]);
         return 1;
     }
-    
-    char *username = argv[3]; // Zapisujemy wskaźnik do nicka
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("socket");
-        return 1;
-    }
-    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
-        perror("signal");
-        close(sock);
+    // validate user input
+    char *endptr;
+    long port = strtol(argv[2], &endptr, 10);
+    if (*endptr != '\0' || port < 1 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %s (must be 1-65535)\n", argv[2]);
         return 1;
     }
 
-    // Ustawienia adresu serwera, do którego się łączymy
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[2]));
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    char *username = argv[3];
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("connect");
-        close(sock);
+    if (setup_tcp((int)port, argv[1]) != 0) {
+        printf("Failed to setup TCP channel!\n");
         return 1;
     }
 
-    printf("Connected to server! Start typing...\n");
-    printf("You: ");
+    if (setup_udp() != 0) {
+        printf("Failed to setup UDP channel!\n");
+        return 1;
+    }
+
+    if (setup_multicast() != 0) {
+        printf("Failed to setup multicast!\n");
+        return 1;
+    }
+
+    printf("Connected to server!\n");
     fflush(stdout);
 
     fd_set read_fds;
 
-    while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(STDIN_FILENO, &read_fds); // add stdin to select set
-        FD_SET(sock, &read_fds); // add TCP sock to select set
+    char buffer[BUF_SIZE];
+    char send_buffer[BUF_SIZE];
 
-        // select block until any fd from read_fds is ready to read
-        if (select(sock + 1, &read_fds, NULL, NULL, NULL) == -1) {
+    while (keep_running) {
+        FD_ZERO(&read_fds);
+        FD_SET(STDIN_FILENO, &read_fds); 
+        FD_SET(sockStream, &read_fds); 
+        FD_SET(sockDgram, &read_fds); 
+        FD_SET(sockMulticast, &read_fds); 
+        
+        int max_fd = sockStream;
+        if (sockDgram > max_fd) max_fd = sockDgram;
+        if (sockMulticast > max_fd) max_fd = sockMulticast;
+
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
+            if (errno == EINTR) {
+                if (!keep_running) break; // FIX #5: explicit check, same as server fix
+                continue; 
+            }
             perror("select failed");
             break;
         }
 
-        // socket
-        if (FD_ISSET(sock, &read_fds)) {
-            int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (FD_ISSET(sockStream, &read_fds)) {
+            int bytes_received = recv(sockStream, buffer, sizeof(buffer) - 1, 0);
             if (bytes_received <= 0) {
                 printf("\nServer disconnected or error.\n");
                 break;
@@ -82,10 +208,34 @@ int main(int argc, char *argv[]) {
             fflush(stdout);
         }
 
-        // stdin
+        if (FD_ISSET(sockDgram, &read_fds)) {
+            int bytes_received = recvfrom(sockDgram, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+            if (bytes_received <= 0) {
+                printf("\nReceive from UDP socket failed.\n");
+                continue;
+            }
+            buffer[bytes_received] = '\0';
+
+            printf("\r[UDP Received]: %s\n", buffer);
+            printf("You: ");
+            fflush(stdout);
+        }
+
+        if (FD_ISSET(sockMulticast, &read_fds)) {
+            int bytes_received = recvfrom(sockMulticast, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+            if (bytes_received <= 0) {
+                printf("\nReceive from Multicast socket failed.\n");
+                continue;
+            }
+            buffer[bytes_received] = '\0';
+
+            printf("\r[Multicast Received]: %s\n", buffer);
+            printf("You: ");
+            fflush(stdout);
+        }
+
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
             if (fgets(buffer, sizeof(buffer) - 1, stdin) != NULL) {
-                // fgets leaves \n at the end of message, replcae it with \0
                 buffer[strcspn(buffer, "\n")] = 0;
 
                 if (strlen(buffer) == 0) {
@@ -94,12 +244,23 @@ int main(int argc, char *argv[]) {
                     continue;
                 }
 
-                char send_buffer[2048];
-                snprintf(send_buffer, sizeof(send_buffer), "[%s]: %s", username, buffer);
-
-                if (send(sock, send_buffer, strlen(send_buffer), 0) == -1) {
-                    perror("send mesage failed");
-                    break;
+                if (strncmp(buffer, CMD_MEDIA, 2) == 0) { 
+                    snprintf(send_buffer, sizeof(send_buffer), "[%s]: %s", username, buffer + 2);
+                    if (sendto(sockDgram, send_buffer, strlen(send_buffer), MSG_NOSIGNAL, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+                        perror("UDP sendto failed");
+                    }
+                } else if (strncmp(buffer, CMD_MULTICAST, 2) == 0) {
+                    snprintf(send_buffer, sizeof(send_buffer), "[%s]: %s", username, buffer + 2);
+                    if (sendto(sockMulticast, send_buffer, strlen(send_buffer), MSG_NOSIGNAL, (struct sockaddr *)&mc_dest_addr, sizeof(mc_dest_addr)) == -1) {
+                        perror("Multicast sendto failed");
+                    }
+                } else { 
+                    snprintf(send_buffer, sizeof(send_buffer), "[%s]: %s", username, buffer);
+                    // MSG_NOSIGNAL prevents SIGPIPE crash on TCP send
+                    if (send(sockStream, send_buffer, strlen(send_buffer), MSG_NOSIGNAL) == -1) {
+                        perror("TCP send message failed");
+                        break;
+                    }
                 }
 
                 printf("You: ");
@@ -108,6 +269,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    close(sock);
+    cleanup();
+    printf("\nClient shutting down. Cleanup finished.\n");
     return 0;
 }
--- distributed_systems/lab1/server.c
+++ distributed_systems/lab1/server.c
@@ -1,39 +1,52 @@
-#include <signal.h> // SIGINT
+#include <signal.h>
 #include <string.h>
-#include <sys/socket.h> // sockets
-#include <stdio.h> // printf
-#include <stdlib.h> // atoi
-#include <unistd.h> // close
-#include <netinet/in.h> // INADDR_ANY
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include <pthread.h>
+#include <errno.h>
 
-#define MAX_CLIENTS 10 // max number of clients in a chat
+#define MAX_CLIENTS 10
 #define BUF_SIZE 2048
 
-int g_server_socket;
-int g_clients[MAX_CLIENTS]; // list of sockets
-int g_clientCount = 0; // number of clients connected, == number of sockets
+volatile sig_atomic_t keep_running = 1; // Async-signal-safe flag for graceful shutdown
+
+int g_server_socket = -1; // Initialized to -1 to avoid closing stdin on early failure
+int g_udp_socket = -1;
+int g_clients[MAX_CLIENTS];
+int g_clientCount = 0;
 
 pthread_mutex_t g_clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void cleanup() {
+    pthread_mutex_lock(&g_clients_mutex);
     for (int i = 0; i < g_clientCount; i++) {
-        close(g_clients[i]);
+        if (g_clients[i] != -1) {
+            close(g_clients[i]);
+            g_clients[i] = -1;
+        }
     }
-    close(g_server_socket);
+    pthread_mutex_unlock(&g_clients_mutex);
 
-    printf("Server shutting down.\n");
+    if (g_server_socket != -1) {
+        close(g_server_socket);
+        g_server_socket = -1;
+    }
+    if (g_udp_socket != -1) {
+        close(g_udp_socket);
+        g_udp_socket = -1;
+    }
+
+    pthread_mutex_destroy(&g_clients_mutex);
 }
 
 void sigint_handler(int sig) {
-    (void)sig; // int sig to satisfy func signature
-
-    cleanup();
-    exit(0);
+    (void)sig; 
+    keep_running = 0; // Defer actual cleanup to the main loop
 }
 
-
-// adds client to clients array, func is idempotent
 int add_client(int socket_fd) {
     pthread_mutex_lock(&g_clients_mutex);
 
@@ -49,7 +62,6 @@ int add_client(int socket_fd) {
     return 0;
 }
 
-// removes client from clients array, func is idempotent
 void remove_client(int socket_fd) {
     pthread_mutex_lock(&g_clients_mutex);
 
@@ -66,20 +78,61 @@ void remove_client(int socket_fd) {
     pthread_mutex_unlock(&g_clients_mutex);
 }
 
-// broadcast sends msg to all clients expect the sender
-void broadcast(char* msg, int sender_socket_fd) {
+void broadcast(const char* msg, int sender_socket_fd) {
     pthread_mutex_lock(&g_clients_mutex);
 
     for (int i = 0; i < g_clientCount; i++) {
         if (g_clients[i] == sender_socket_fd) {
             continue;
         }
-        if (send(g_clients[i], msg, strlen(msg), 0) == -1) {
+        
+        // MSG_NOSIGNAL prevents SIGPIPE crash when a client disconnects
+        // during broadcast — without this, the server would crash on broken pipe.
+        if (send(g_clients[i], msg, strlen(msg), MSG_NOSIGNAL) == -1) {
             perror("broadcast send failed");
         }
     }
 
     pthread_mutex_unlock(&g_clients_mutex);
+}
+
+void *handle_udp_traffic(void *arg) {
+    (void)arg;
+    char buffer[BUF_SIZE];
+    struct sockaddr_in sender_addr;
+    socklen_t sender_len = sizeof(sender_addr);
+
+    while (keep_running) {
+        int bytes_received = recvfrom(g_udp_socket, buffer, sizeof(buffer) - 1, 0, (struct sockaddr*)&sender_addr, &sender_len);
+        if (bytes_received <= 0) {
+            if (!keep_running) break; // Exit cleanly if interrupted by shutdown
+            continue;
+        }
+
+        buffer[bytes_received] = '\0';
+        printf("[SERVER UDP ROUTER]: Received a packet, broadcasting...\n");
+
+        pthread_mutex_lock(&g_clients_mutex);
+        for (int i = 0; i < g_clientCount; i++) {
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+
+            if (getpeername(g_clients[i], (struct sockaddr*)&client_addr, &client_len) == -1) {
+                printf("[SERVER UDP ERR]: failed to get peer name\n");
+                continue;
+            }
+
+            if (client_addr.sin_port == sender_addr.sin_port && client_addr.sin_addr.s_addr == sender_addr.sin_addr.s_addr) {
+                continue;
+            }
+
+            if (sendto(g_udp_socket, buffer, bytes_received, 0, (struct sockaddr*)&client_addr, client_len) == -1) {
+                perror("UDP broadcast sendto failed");
+            }
+        }
+        pthread_mutex_unlock(&g_clients_mutex);
+    }
+    return NULL;
 }
 
 void *handle_client(void* arg) {
@@ -88,9 +141,7 @@ void *handle_client(void* arg) {
 
     char buffer[BUF_SIZE];
 
-    while (1) {
-        memset(buffer, 0, sizeof(buffer)); // clear buffor before read
-
+    while (keep_running) {
         int bytes_received = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
         if (bytes_received <= 0) {
             printf("Client (Socket fd: %d) disconnected.\n", client_sock);
@@ -110,12 +161,17 @@ void *handle_client(void* arg) {
     return NULL;
 }
 
-
-
 int main(int argc, char *argv[]) {
-    // accept port as param
     if (argc < 2) {
         printf("Usage: %s <server_port>\n", argv[0]);
+        return 1;
+    }
+
+    // validate user input
+    char *endptr;
+    long port = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || port < 1 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %s (must be 1-65535)\n", argv[1]);
         return 1;
     }
 
@@ -124,39 +180,69 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Ignore SIGPIPE globally so broken client connections don't crash the server
+    signal(SIGPIPE, SIG_IGN);
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr)); // Zero out padding to avoid garbage values
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons((uint16_t)port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
     g_server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (g_server_socket == -1) {
         perror("server socket");
         return 1;
     }
-
-    // Faster cleanup of socket, normally need to wait around 60s for system cleanup
-    int opt = 1;
+    
+    int opt = 1; 
     setsockopt(g_server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[1])); // this mmight use incorrect pointer semantics!
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
     if (bind(g_server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
         perror("bind server socket");
         cleanup();
         return 1;
     }
-
     if (listen(g_server_socket, MAX_CLIENTS) != 0) {
         perror("listen");
         cleanup();
         return 1;
     }
 
+    g_udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (g_udp_socket == -1) {
+        perror("udp socket");
+        cleanup();
+        return 1;
+    }
+
+    int opt_udp = 1; 
+    setsockopt(g_udp_socket, SOL_SOCKET, SO_REUSEADDR, &opt_udp, sizeof(opt_udp));
+    if (bind(g_udp_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+        perror("bind udp socket");
+        cleanup();
+        return 1;
+    }
+
+    pthread_t udp_thread;
+    if (pthread_create(&udp_thread, NULL, handle_udp_traffic, NULL) != 0) {
+        perror("udp thread creation");
+        cleanup();
+        return 1;
+    }
+    pthread_detach(udp_thread);
+
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    while (1) {
+    printf("Server started on port %ld. Waiting for connections...\n", port);
+
+    while (keep_running) {
         int client_sock = accept(g_server_socket, (struct sockaddr *)&client_addr, &client_len);
         if (client_sock == -1) {
+            if (errno == EINTR) {
+                if (!keep_running) break;
+                continue;
+            }
             perror("accept failed");
             continue;
         }
@@ -170,10 +256,15 @@ int main(int argc, char *argv[]) {
         printf("New client connected! Socket fd: %d\n", client_sock);
 
         int *new_sock = malloc(sizeof(int));
+        if (new_sock == NULL) {
+            perror("malloc failed");
+            remove_client(client_sock);
+            close(client_sock);
+            continue;
+        }
         *new_sock = client_sock;
 
         pthread_t thread_id;
-
         if (pthread_create(&thread_id, NULL, handle_client, (void *)new_sock) != 0) {
             perror("thread creation");
             free(new_sock);
@@ -186,5 +277,6 @@ int main(int argc, char *argv[]) {
     }
 
     cleanup();
+    printf("Server shutting down cleanly.\n");
     return 0;
 }
